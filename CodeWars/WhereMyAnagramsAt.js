6f08035a0db6d9e759e3b7a543315628f8fd5564
Fernando1Borges <centraloro@gmail.com>
2016-12-10 12:35:44 -0600

    Add anagrams kata progress

--- CodeWars/WhereMyAnagramsAt.js
+++ CodeWars/WhereMyAnagramsAt.js
@@ -1,22 +1,8 @@
 function anagrams(word, words) {
   let inputWord = word.split(''),
       outputArray = [],
-      inputArray = words;
+      inputArray = ['aabb', 'bbaa'];
 
-      return inputWord;
-
-
-
+      return inputArray;
 
 }
-
-describe("Basic Tests", function(){
-
-  it("More basic tests", function(){
-
-      Test.assertEquals(anagrams("mocha", ["mchoa", "mokoa"]), "mchoa");
-      Test.assertEquals(anagrams("car", ["cat", "dog"]), []);
-      Test.assertEquals(anagrams("chai", ["iach", "chacha", "haiku", "haic"]), ["iach", "haic"]);
-
-  })
-});
