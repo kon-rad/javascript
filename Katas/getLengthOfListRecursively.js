10be3c1ff716b1b3216f5478970548188c4d5412
Fernando1Borges <centraloro@gmail.com>
2016-12-15 08:33:36 -0600

    Add list recursive kata

---
+++ Katas/getLengthOfListRecursively.js
@@ -0,0 +1,22 @@
+// 7kyu Kata Completed 12/15/16
+
+function lenR(x) {
+  if (!x[0]) {
+    return 0;
+  } else {
+    if (Array.isArray(x)) {
+      x.splice(-1,1);
+      return 1  + (lenR(x));
+    } else {
+       x = x.split('');
+       x.splice(-1,1);
+       return 1  + (lenR(x));
+    }
+  }
+}
+
+// Example Tests
+
+//lenR([]); // should return 0
+//lenR([2,1]); // should return 2
+lenR("hello"); // should return 5
