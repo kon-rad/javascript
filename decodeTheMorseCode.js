f134bc9dda026de41c42a20ee66373fdf12142cc
Fernando1Borges <centraloro@gmail.com>
2016-12-23 08:20:25 -0600

    Add caesar cipher progress and modify decode the mores code

--- decodeTheMorseCode.js
+++ decodeTheMorseCode.js
@@ -14,3 +14,21 @@ decodeMorse = function(morseCode){
   });
   return result.join('').trim();
 }
+
+// Test cases
+decodeMorse('.... . -.--   .--- ..- -.. .')// should return 'HEY JUDE')
+
+
+
+// Other Solutions
+// Credit: czyzykowski, RJackson88
+//
+// decodeMorse = function(morseCode){
+//   function decodeMorseLetter(letter) {
+//     return MORSE_CODE[letter];
+//   }
+//   function decodeMorseWord(word) {
+//     return word.split(' ').map(decodeMorseLetter).join('');
+//   }
+//   return morseCode.trim().split('   ').map(decodeMorseWord).join(' ');
+// }
