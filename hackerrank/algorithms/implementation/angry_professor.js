e69a84c94410ede4c431538783440016f42e4237
konradgnat <konradmgnat@gmail.com>
2017-06-01 07:58:54 -0500

    add angry professor

---
+++ hackerrank/algorithms/implementation/angry_professor.js
@@ -0,0 +1,48 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var t = parseInt(readLine());
+    for(var a0 = 0; a0 < t; a0++){
+        var n_temp = readLine().split(' ');
+        var n = parseInt(n_temp[0]);
+        var k = parseInt(n_temp[1]);
+        a = readLine().split(' ');
+        a = a.map(Number);
+        var on_time = 0, cancelled = true;
+        for(var i = 0; i < a.length; i++){
+            if(a[i] <= 0){
+                on_time++;
+                if(on_time >= k){
+                    cancelled = false;
+                    break;
+                }
+            }
+        }
+        if(cancelled){
+            console.log("YES");
+        } else {
+            console.log("NO");
+        }
+    }
+
+}
