f72b0ae88652e28b8cc40aa77e16cd4f4babfb3c
konradgnat <konradmgnat@gmail.com>
2017-06-05 08:42:40 -0500

    add jumping on the clouds

---
+++ hackerrank/algorithms/implementation/jumping_on_the_clouds.js
@@ -0,0 +1,41 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n_temp = readLine().split(' ');
+    var n = parseInt(n_temp[0]);
+    var k = parseInt(n_temp[1]);
+    c = readLine().split(' ');
+    c = c.map(Number);
+    var position = 0,
+        e = 100;
+    while(position < n){
+        position = (position + k)%n;
+        if(c[position] == 1){
+            e -= 2;
+        }
+        e -= 1;
+        if(position == 0)
+            break;
+    }
+    console.log(e);
+}
