0a6adbf02b505a5375d2e64a547b6409757300a1
konradgnat <konradmgnat@gmail.com>
2017-06-08 10:29:50 -0500

    Add append and delete java version and gitignore

--- hackerrank/algorithms/implementation/append_and_delete.java
+++ hackerrank/algorithms/implementation/append_and_delete.java
@@ -15,30 +15,27 @@ public class Solution {
 
         int commonLength = 0;
         
-        for (int i=0; i<java.lang.Math.min(s.length(),t.length());i++){
-            if (s.charAt(i)==t.charAt(i))
-                commonLength++;
+        for(int i = 0; i < java.lang.Math.min(s.length(), t.length()); i++){
+            if(s.charAt(i) == t.charAt(i))
+              commonLength++;
             else
                 break;
         }
-        //CASE A
-        if((s.length()+t.length()-2*commonLength)>k){
-            System.out.println("No");
+        // Case 1
+        if ((s.length() + t.length() - 2*commonLength)>k){
+          System.out.println("No");
         }
-        //CASE B
-        else if((s.length()+t.length()-2*commonLength)%2==k%2){
-            System.out.println("Yes");
+        // Case 2
+        else if ((s.length() + t.length() - 2*commonLength)%2 ==k%2){
+          System.out.println("Yes");
         }
-        //CASE C
-        else if((s.length()+t.length()-k)<0){
-            System.out.println("Yes");
+        // Case 3
+        else if ((s.length() + t.length()-k)<0){
+          System.out.println("Yes");
         }
-        //CASE D
-        else{
-            System.out.println("No");
-        }
+        // Case 4
+        else {
+          System.out.println("No");
+        }        
     }
 }
-
-
-
