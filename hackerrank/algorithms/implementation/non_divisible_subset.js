f475ec517d0cdc3ea9b6c8f4ca5274b912b28bd8
konradgnat <konradmgnat@gmail.com>
2017-06-21 10:48:16 -0500

    Add soution to non divisible subset

--- hackerrank/algorithms/implementation/non_divisible_subset.js
+++ hackerrank/algorithms/implementation/non_divisible_subset.js
@@ -1,57 +1,37 @@
-function is_divisible_by(num1, num2){
-  if (num1 % num2 === 0)
-    return true;
-  return false;
-}
-function getCombinations(arr, k) {
-  var result = [];
-  var f = function(prefix, arr) {
-    for (var i = 0; i < arr.length; i++) {
-      if(test_for_divisibility(prefix + " " + arr[i], k)) {
-        result.push(prefix + " " + arr[i]);
-        f(prefix + " " + arr[i], arr.slice(i + 1));
-      }
-    }
-  }
-  f('', arr);
-  return result;
-}
-function test_for_divisibility(arr, k){
-    arr = arr.split(' ');
-    for(var i = 0; i < arr.length; i++){
-        for(var j = 0; j < arr.length; j++){
-            var divisible = ((Number(arr[i]) + Number(arr[j])) % k === 0);
-            if (i != j && divisible){
-                    return false;
-            } 
-        }
-    }
-    return arr.length;
-}
+
 function processData(input) {
-    //Enter your code here
     input = input.split('\n');
     var k = input[0].split(' ');
-    k = k[1];
-    var arr = input[1].split(' ');
-    var combinations = getCombinations(arr, k);
-    var lengths = [];
-    
-    console.log("combinations = " + combinations);
-    if (combinations.length === 0){
-        return console.log(1);
-    }
-    combinations.forEach(function(x){
-        var split_array = x.split(' ');
-        //console.log("split array = " + split_array);
-        lengths.push(split_array.length - 1);
-        //if(test_for_divisibility(x, k)){
-          //  lengths.push(test_for_divisibility(x, k));
-        //} 
+    k = Number(k[1]);
+    var arr = input[1].split(' ').map(Number);
+    var mod_arr = new Array(k + 1).fill(0),
+        count = 0;
+    arr.forEach(function(num) {
+        var pos = num % k;
+        mod_arr[pos]++;
     });
-    //console.log(lengths);
-    console.log(Math.max.apply(null, lengths));
-} 
+    if (k % 2 === 0) {
+        for(var i = 0; i <= (k / 2); i++) {
+            if (i === 0 && mod_arr[i] > 0) {
+                count++;
+            } else if (i === (k / 2)) {
+                count++;
+            } else {
+                count += Math.max(mod_arr[i], mod_arr[k - i]);
+            }
+        }
+    } else {
+        for(var i = 0; i <= Math.floor(k/2); i++) {
+            if (i === 0 && mod_arr[i] > 0) {
+                count++;
+            } else {
+                count += Math.max(mod_arr[i], mod_arr[k - i]);
+            }
+        }   
+    }
+    console.log((count == 0) ? 1 : count);
+    
+} ;
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
 _input = "";
