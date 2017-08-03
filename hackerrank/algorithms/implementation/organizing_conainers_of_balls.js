f1f3f43910c231d5f775dc074442979b87f17dbf
konradgnat <konradmgnat@gmail.com>
2017-08-03 08:16:46 -0500

    Add organizing containers progress

--- hackerrank/algorithms/implementation/organizing_conainers_of_balls.js
+++ hackerrank/algorithms/implementation/organizing_conainers_of_balls.js
@@ -20,6 +20,17 @@ function readLine() {
 
 /////////////// ignore above this line ////////////////////
 
+function check_matrix(M, n){
+    for(var i = 0; i < n; i++) {
+        for(var j = 0; j < n; j++) {
+            if(i != j && M[i][j]) {
+                return 'Impossible';
+            }
+        }
+    }
+    return 'Possible';
+}
+
 function main() {
     var q = parseInt(readLine());
     for(var a0 = 0; a0 < q; a0++){
@@ -30,6 +41,19 @@ function main() {
            M[M_i] = M[M_i].map(Number);
         }
         // your code goes here
+        
+        for(var i = 0; i < n; i++) {
+            for(var j = 0; j < n; j++) {
+                if(M[j][i]) {
+                    if(M[i][j]) {
+                        var swap = M[j][i];
+                        M[i][i] += swap;
+                        M[j][i] -= swap;
+                    }
+                }
+            }
+        }
+        console.log(M);
+        console.log(check_matrix(M, n));
     }
-
 }
