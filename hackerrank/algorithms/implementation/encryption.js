e15c81a098d87269db6e9ea0eabca6aa4c1c8586
konradgnat <konradmgnat@gmail.com>
2017-08-09 08:25:05 -0500

    Add encryption solution

---
+++ hackerrank/algorithms/implementation/encryption.js
@@ -0,0 +1,40 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var s = readLine();
+    s = s.split('');
+    var length = s.length;
+    var row = Math.floor(Math.sqrt(length));
+    var col = Math.ceil(Math.sqrt(length));
+    var encrypted = [];
+    if (row*col < length) {
+        row += 1;
+    }
+    for(var i = 0; i < col; i++){
+        for(var j = 0; j < row; j++){
+            encrypted.push(s[(j*col)+i]);
+        }
+        encrypted.push(' ');
+    }
+    console.log(encrypted.join(''));
+}
