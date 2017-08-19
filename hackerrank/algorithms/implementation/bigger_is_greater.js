e3a1e0801b3ec8283e02a343d72f670e2cc8aaa4
konradgnat <konradmgnat@gmail.com>
2017-08-19 14:36:21 -0500

    Add next permutations function to bigger is

--- hackerrank/algorithms/implementation/bigger_is_greater.js
+++ hackerrank/algorithms/implementation/bigger_is_greater.js
@@ -1,4 +1,47 @@
+function run(chunk) {
+    var arr=chunk.split('');
+    
+    var cnt=0;
+    do{
+        if(cnt>0)
+          return arr;
+        ++cnt;
+    }while(next_permutation(arr,function(a,b){return a<b;}));
+}
 
+
+function next_permutation(arr, pred) {
+    if (!arr.length|arr.length===1) return false;   
+    var last = arr.length-1;
+    var i=last;
+    
+    function rev(beg,en){
+        var mid=(en-beg)>>1;
+        for(var x=0;x<=mid;++x){
+            var tmp=arr[beg+x];
+            arr[beg+x]=arr[en-x];
+            arr[en-x]=tmp;
+        }
+    }
+ 
+    for(;;) {
+        var ii = i--;
+        if (pred(arr[i],arr[ii])) {
+            var j = last+1;
+            while (!pred(arr[i],arr[--j]));
+            var tmp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=tmp; 
+            
+            rev(ii, last);
+            return true;
+        }
+        if (i === 0) { 
+            rev(0, last);
+            return false;
+        }
+    }
+}
 function permut(arr) {
   if(arr.length < 2) return arr;
   var permutations = [];
@@ -29,11 +72,15 @@ function next_greatest(org) {
     for (var i = len-1; i >= 0; i--) {
           if (org.charAt(i) < highest) {
               var end = org.slice(i, len);
+              new_combo = run(end);
+              return org.slice(0, i) + new_combo.join('');
+              /*
               new_combo = permut(end);
               var sorted = order(new_combo);
               if (sorted.indexOf(end) != sorted.length-1) {
                 return org.slice(0, i) + sorted[sorted.indexOf(end)+1];
               }
+              */
           } else if (org.charAt(i) > highest) {
               highest = org.charAt(i);
           }
