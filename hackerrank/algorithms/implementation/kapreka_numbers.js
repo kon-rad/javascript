0a7dfc0a1140c59cc0a02a2c21ec025df4e26a81
konradgnat <konradmgnat@gmail.com>
2017-08-24 08:01:05 -0500

    Add kapreka solution

--- hackerrank/algorithms/implementation/kapreka_numbers.js
+++ hackerrank/algorithms/implementation/kapreka_numbers.js
@@ -1,9 +1,17 @@
+
+var answer = [];
+
 function processData(input) {
     //Enter your code here
     input = input.split('\n');
-    var s = input[0];
-    var e = input[1];
+    var s = parseInt(input[0]);
+    var e = parseInt(input[1]);
     find_kaprekar_nums(s, e);
+      if (answer.length === 0) {
+    console.log("INVALID RANGE");
+  } else {
+    console.log(answer.join(' '));
+  }
 } 
 
 function find_kaprekar_nums(s, e) {
@@ -13,9 +21,30 @@ function find_kaprekar_nums(s, e) {
 }
 
 function is_kaprekar(i) {
+  var c = i;
   var s = i*i;
-  s = s.toString().split('');
-  console.log(s);
+  //console.log(s);
+  s = s.toString().split('');a
+  var len = s.length;
+  var h = Math.floor(len/2);
+  var f = s.splice(0, h);
+  var t = s;
+  f = parseInt(f.join(''));
+  t = parseInt(t.join(''));
+  if(!f) f=0;
+  if(f+t == c && c!==0) {
+    answer.push(c);
+  }
+
 }
-var data = "1\n50";
-processData(data);
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
