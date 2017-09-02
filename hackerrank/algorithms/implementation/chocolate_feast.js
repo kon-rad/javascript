4df8937f6a4de1fca1fceaac891df614190aa320
konradgnat <konradmgnat@gmail.com>
2017-09-02 08:51:01 -0500

    Add choc

---
+++ hackerrank/algorithms/implementation/chocolate_feast.js
@@ -0,0 +1,42 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var t = parseInt(readLine());
+    for(var a0 = 0; a0 < t; a0++){
+        var n_temp = readLine().split(' ');
+        var n = parseInt(n_temp[0]);
+        var c = parseInt(n_temp[1]);
+        var m = parseInt(n_temp[2]);
+        
+        // buy chocalates with c
+        var choc = Math.floor(n/c);
+        var wrap = choc;
+        while (wrap >= m) {
+            var remains = wrap%m;
+            var newChoc = Math.floor(wrap/m);
+            wrap = newChoc+remains;
+            choc += newChoc;
+        }
+        console.log(choc);
+    }
+}
