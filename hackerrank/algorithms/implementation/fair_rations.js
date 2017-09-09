ae684a34b08f3db652d69579922d456909184519
konradgnat <konradmgnat@gmail.com>
2017-09-09 08:08:10 -0500

    Add fair rations

--- hackerrank/algorithms/implementation/fair_rations.js
+++ hackerrank/algorithms/implementation/fair_rations.js
@@ -29,36 +29,16 @@ function main() {
     B = readLine().split(' ');
     B = B.map(Number);
     var count = 0;
-    var secondToLast;
-    var curE,
-        len = B.length;
-    for(var i = 0; i < len; i++) {
-        secondToLast = (i === len-2);
-        curE = isEven(B[i]);
-        nxtE = isEven(B[i+1]);
-        if (i === len) {
-            if(!curE){
-                return console.log('NO');
-            } else {
-                continue;
-            }
-        } else if(secondToLast && !curE && nxtE) {
-             return console.log('NO');
-        } else if ((secondToLast && !curE) && !nxtE) {
-                B[i]++;
-                B[i+1]++;
-                count += 2;
-        } else if (!curE){
-            if((!secondToLast && nxtE && !isEven(B[i+2])) || !nxtE){
-                B[i]++;
-                B[i+1]++;
-                count += 2;
-            } else {
-                B[i]++;
-                B[i+1]++;
-                count += 2;
-            }
+    for(var i = 0; i < N-1; i++) {
+        if(!isEven(B[i])){
+            B[i]++;
+            B[i+1]++;
+            count+=2;
         }
     }
-    console.log(count);
+    if(isEven(B[N-1]))
+        console.log(count);
+    else
+        console.log('NO');
+
 }
