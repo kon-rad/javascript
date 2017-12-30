bb2cc99eb3db5d12b1dad7c920eb8400bdb10b59
konradgnat <konradmgnat@gmail.com>
2017-12-30 11:51:11 -0600

    clean code

--- hackerrank/ctci/comparotorSorting.py
+++ hackerrank/ctci/comparotorSorting.py
@@ -9,15 +9,13 @@ class Player:
     def comparator(a, b):
         if a.score < b.score:
             return 1
-        elif a.score > b.score:
+        if a.score > b.score:
             return -1
-        else :
-            if a.name < b.name:
-                return -1
-            elif a.name > b.name:
-                return 1
-            else :
-                return 0
+        if a.name < b.name:
+            return -1
+        if a.name > b.name:
+            return 1
+        return 0
 
 n = int(input())
 data = []
